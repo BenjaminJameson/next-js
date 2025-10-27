336e61438ab669a83560e1e2a5a250c7609a76ec
Jackson hill <hilljackson820@gmail.com>
2025-10-27 19:09:23 +1100

    Added keyboard spam to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentationhhnhhh
+            Documentationhhnhhhr
           </a>
         </div>
       </main>
